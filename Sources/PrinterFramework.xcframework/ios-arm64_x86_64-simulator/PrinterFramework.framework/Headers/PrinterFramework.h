b027521d47631e883eb4c1c5b8c77d1957ae4cd9
xund-novak <83659015+xund-novak@users.noreply.github.com>
2023-09-13 23:00:28 +0200

    Add files via upload

---
+++ Sources/PrinterFramework.xcframework/ios-arm64_x86_64-simulator/PrinterFramework.framework/Headers/PrinterFramework.h
@@ -0,0 +1,18 @@
+//
+//  PrinterFramework.h
+//  PrinterFramework
+//
+//  Created by Dániel Novák on 2023. 09. 12..
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PrinterFramework.
+FOUNDATION_EXPORT double PrinterFrameworkVersionNumber;
+
+//! Project version string for PrinterFramework.
+FOUNDATION_EXPORT const unsigned char PrinterFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PrinterFramework/PublicHeader.h>
+
+
